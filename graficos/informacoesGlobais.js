7ea428e9edfc3336c216c7c693820f75ec03869e
Marcelo Yukio Yamamoto <86316144+Marceloyy51@users.noreply.github.com>
2024-10-30 21:04:19 +0000

    Alteração 30.10.2024_Prof.Ronaldo

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,14 +3,17 @@ const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glo
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-
     //console.log(dados)
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `No mundo, existem cerca de ${dados.total_pessoas_mundo} de pessoas, das quais a maioria (6    bilhões) têm acesso à educação. <br>No entanto, a quantidade de horas dedicadas ao estudo ainda é pequena.
+    paragrafo.innerHTML = 
+                `No mundo, existem cerca de <span>${dados.total_pessoas_mundo}</span> de pessoas, das quais a maioria (<span>6    bilhões</span>) têm acesso à educação. 
+                <br>No entanto, a quantidade de horas dedicadas ao estudo ainda é pequena.
                 <br>João, por exemplo, sempre sonhou em cursar uma faculdade, mas, como muitos, precisou trabalhar desde cedo e só conseguia estudar poucas horas por dia.
-                <br>Apesar das dificuldades, ele se dedicava como podia, sabendo que apenas uma pequena parte da população, cerca de 1,5 bilhão de pessoas, chega ao ensino superior.
-                <br>João acreditava que, com esforço, poderia fazer parte desse grupo. Mesmo com pouco tempo, ele estudava 8 horas diariamente, enquanto a média global é de cerca de 4 horas. 
+                <br>Apesar das dificuldades, ele se dedicava como podia, sabendo que apenas uma pequena parte da população, cerca de <span>1,5 bilhão</span> de pessoas, chega ao ensino superior.
+                <br>João acreditava que, com esforço, poderia fazer parte desse grupo. 
+                <br>Mesmo com pouco tempo, ele estudava <span>8 horas</span> diariamente, enquanto a média global é de cerca de <span>4 horas</span>. 
                 <br>Assim, João obteve sucesso e conseguiu realizar seu sonho.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
