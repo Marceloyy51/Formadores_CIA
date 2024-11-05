4787f2e218fcf0caade360df1f9c43ba13400d38
Marcelo Yukio Yamamoto <86316144+Marceloyy51@users.noreply.github.com>
2024-11-05 19:46:47 +0000

    aula6

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,20 +1,22 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/esportes/esportes-dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     //console.log(dados)
 
+    const pessoasMundo = (dados.total_pessoas_mundo / 1e9)
+    const pessoasPraticam = (dados.total_pessoas_que_praticam_esportes_regularmente / 1e9)
+    const pessoasAssistem = (dados.total_pessoas_que_assistem_esportes / 1e9)
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = 
-                `No mundo, existem cerca de <span>${dados.total_pessoas_mundo}</span> de pessoas, das quais a maioria (<span>6    bilhões</span>) têm acesso à educação. 
-                <br>No entanto, a quantidade de horas dedicadas ao estudo ainda é pequena.
-                <br>João, por exemplo, sempre sonhou em cursar uma faculdade, mas, como muitos, precisou trabalhar desde cedo e só conseguia estudar poucas horas por dia.
-                <br>Apesar das dificuldades, ele se dedicava como podia, sabendo que apenas uma pequena parte da população, cerca de <span>1,5 bilhão</span> de pessoas, chega ao ensino superior.
-                <br>João acreditava que, com esforço, poderia fazer parte desse grupo. 
-                <br>Mesmo com pouco tempo, ele estudava <span>8 horas</span> diariamente, enquanto a média global é de cerca de <span>4 horas</span>. 
-                <br>Assim, João obteve sucesso e conseguiu realizar seu sonho.`
+                `Com uma população mundial estimada em <span>${pessoasMundo}</span> bilhão de pessoas, o esporte se mostra como uma paixão global.
+                <br>Os dados revelam que <span>${pessoasPraticam}</span> bilhões de pessoas dedicam parte do seu tempo livre à prática regular de atividades físicas, representando quase metade da população mundial. Em média, essas pessoas investem <span>${dados.tempo_medio_semana_praticando_esportes}</span> horas por semana em esportes.
+                <br>Além de praticar, <span>${pessoasAssistem}</span> bilhões de pessoas se declaram acompanhar jogos e competições. Esses números demonstrar o enorme potencial do setor esportivo, que movimenta bilhões de dólares e gera empregos em todo o mundo.
+                <br>O esporte é mais do que uma simples atividade de lazer. É um fenômeno global que une pessoas, culturas e economias. No entanto, apesar dos números expressivos, ainda há desafios a serem superados, como a desigualdade de acesso ao esporte em algumas regiões e a necessidade de políticas públicas que incentivem a prática esportiva.
+                `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
